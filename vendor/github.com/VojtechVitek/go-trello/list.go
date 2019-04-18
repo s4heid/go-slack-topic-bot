f78a1c6b1a5affee8db753d793a4b020d326104e
Sebastian Heid <sebastian.heid@sap.com>
2019-04-19 01:22:13 +0200

    Run dep ensure

---
+++ vendor/github.com/VojtechVitek/go-trello/list.go
@@ -0,0 +1,67 @@
+/*
+Copyright 2014 go-trello authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package trello
+
+import (
+	"encoding/json"
+)
+
+type List struct {
+	client  *Client
+	Id      string  `json:"id"`
+	Name    string  `json:"name"`
+	Closed  bool    `json:"closed"`
+	IdBoard string  `json:"idBoard"`
+	Pos     float32 `json:"pos"`
+}
+
+func (c *Client) List(listId string) (list *List, err error) {
+	body, err := c.Get("/lists/" + listId)
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(body, &list)
+	list.client = c
+	return
+}
+
+func (l *List) Cards() (cards []Card, err error) {
+	body, err := l.client.Get("/lists/" + l.Id + "/cards")
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(body, &cards)
+	for i := range cards {
+		cards[i].client = l.client
+	}
+	return
+}
+
+func (l *List) Actions() (actions []Action, err error) {
+	body, err := l.client.Get("/lists/" + l.Id + "/actions")
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(body, &actions)
+	for i := range actions {
+		actions[i].client = l.client
+	}
+	return
+}
